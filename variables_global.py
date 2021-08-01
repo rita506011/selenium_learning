349a08efc211e5a12f2cec46bf1dbb31199133cc
Fina Chen <rita506011@gmail.com>
2021-08-01 21:40:29 +0800

    Add testsuite 'SeleniumEasyInit'

---
+++ variables_global.py
@@ -0,0 +1,6 @@
+INT_IMPLICITLY_TIMEOUT = 3
+INT_EXPLICITLY_TIMEOUT = 10
+
+STR_CONFIG_FILE = 'logging.yaml'
+STR_URL_SELENIUM_EASY_DEMO = 'https://www.seleniumeasy.com/test/'
+STR_XPATH_BTN_WELCOME_OPTION_NO = '//*[@id="at-cv-lightbox-button-holder"]/a[2]'
