7b6345ddd7b256ddbf438bbf7db2f4356a0687de
Fina Chen <rita506011@gmail.com>
2021-08-31 21:18:26 +0800

    Implement testcases - basic - Check Box Demo

--- test_basic_level.py
+++ test_basic_level.py
@@ -78,6 +78,80 @@ class TestBasicLevel(SeleniumEasyInit):
         self.logger.debug('Get text result: %s' % get_res)
         assert invalid_res == get_res
 
+    def test_check_box_single(self):
+        self.browser_controller.buttonClick(STR_XPATH_BTN_CHECK_BOX)
+        # check default status
+        checkbox_status = self.browser_controller.isCheckedCheckbox(STR_XPATH_CHECKBOX_CHECK_SINGLE_CHECKBOX)
+        self.logger.debug('Checkbox default status is %s.' % ('checked' if checkbox_status else 'unchecked'))
+        assert checkbox_status is False, 'Default value is wrong, should be unchecked.'
+        # checked checkbox
+        check_res = 'Success - Check box is checked'
+        self.browser_controller.buttonClick(STR_XPATH_CHECKBOX_CHECK_SINGLE_CHECKBOX)
+        get_msg = self.browser_controller.getText(STR_XPATH_CHECKBOX_CHECK_SINGLE_SHOW_MSG)
+        self.logger.debug('Message of checkbox is selected: %s.' % get_msg)
+        assert check_res == get_msg, 'Checkbox selected message is wrong.'
+        # unchecked checkbox
+        self.browser_controller.buttonClick(STR_XPATH_CHECKBOX_CHECK_SINGLE_CHECKBOX)
+        get_msg = self.browser_controller.getText(STR_XPATH_CHECKBOX_CHECK_SINGLE_SHOW_MSG)
+        self.logger.debug('Checkbox unselect message: %s.' % get_msg)
+        assert '' == get_msg, 'Checkbox unselected message is wrong.'
 
+    def test_check_box_multiple_default_check(self):
+        self.browser_controller.buttonClick(STR_XPATH_BTN_CHECK_BOX)
+        get_checked_list = self._check_box_is_checked()
+        assert not get_checked_list, f'At least one checkbox is checked. Checked list is {get_checked_list}.'
 
+    def test_check_box_multiple_button(self):
+        self.browser_controller.buttonClick(STR_XPATH_BTN_CHECK_BOX)
+        btn_check_all_text = 'Check All'
+        btn_uncheck_all_text = 'Uncheck All'
+        checkbox_checked_all_list = [1, 2, 3, 4]
 
+        # click 'Check All' (button show 'Uncheck all')
+        self.logger.info('Click "Check All" button.')
+        self.browser_controller.buttonClick(STR_XPATH_BTN_CHECK_MULTIPLE_ALL)
+        get_checked_list = self._check_box_is_checked()
+        get_btn_value = self.browser_controller.getAttribute(STR_XPATH_BTN_CHECK_MULTIPLE_ALL, 'value')
+        assert get_checked_list == checkbox_checked_all_list, f'At least one checkbox is unchecked. Checked: {get_checked_list}'
+        assert get_btn_value == btn_uncheck_all_text, f'Button text is not "{btn_uncheck_all_text}". Actual result is "{get_btn_value}".'
+
+        # Check/unchecked each checkbox will influence button text
+        self.logger.info('Start to check each checkbox.')
+        for option in range(1, 5):
+            self.logger.debug(f'Unchecked checkbox {option}.')
+            checked_list = checkbox_checked_all_list.copy()
+            # Uncheck checkbox
+            self.browser_controller.buttonClick(globals()[f'STR_XPATH_CHECKBOX_CHECK_MULTIPLE_OPTION_{option}'])
+            checked_list.remove(option)
+            get_checked_list = self._check_box_is_checked()
+            get_btn_value = self.browser_controller.getAttribute(STR_XPATH_BTN_CHECK_MULTIPLE_ALL, 'value')
+            assert get_checked_list == checked_list,\
+                                       f'Checkbox list is wrong. Actual list is {get_checked_list}.'
+            assert get_btn_value == btn_check_all_text, f'Button text isn\'t "{btn_check_all_text}". Actual result is "{get_btn_value}".'
+            # Checked checkbox again
+            self.browser_controller.buttonClick(globals()[f'STR_XPATH_CHECKBOX_CHECK_MULTIPLE_OPTION_{option}'])
+            get_btn_value = self.browser_controller.getAttribute(STR_XPATH_BTN_CHECK_MULTIPLE_ALL, 'value')
+            assert get_btn_value == btn_uncheck_all_text,\
+                                    f'Button text isn\'t "{btn_uncheck_all_text}". Actual result is "{get_btn_value}".'
+
+        # click 'Uncheck All' (button show 'Check all')
+        self.logger.info('Click "Uncheck All" button.')
+        self.browser_controller.buttonClick(STR_XPATH_BTN_CHECK_MULTIPLE_ALL)
+        get_checked_list = self._check_box_is_checked()
+        get_btn_value = self.browser_controller.getAttribute(STR_XPATH_BTN_CHECK_MULTIPLE_ALL, 'value')
+        assert not get_checked_list, f'At least one checkbox is checked. Checked: {get_checked_list}'
+        assert get_btn_value == btn_check_all_text, f'Button text is not "{btn_check_all_text}". Actual result is "{get_btn_value}".'
+
+    def _check_box_is_checked(self):
+        """
+        For form 'Check Box Demo', return a list that checkbox is checked.
+        """
+        checked_list = []
+        for option in range(1, 5):
+            checkbox_status = self.browser_controller. \
+                isCheckedCheckbox(globals()[f'STR_XPATH_CHECKBOX_CHECK_MULTIPLE_OPTION_{option}'])
+            self.logger.debug('Checkbox option %d status is %s.'
+                              % (option, ('checked' if checkbox_status else 'unchecked')))
+            if checkbox_status is True:
+                checked_list.append(option)
+        return checked_list
