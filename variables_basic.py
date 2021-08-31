511cf7ad36852ea36484b7330cc88462928231a9
Fina Chen <daykfa5@gmail.com>
2021-08-31 21:23:29 +0800

    Implement testcases - basic - Check Box Demo (#2)
    Co-authored-by: Fina Chen <rita506011@gmail.com>

--- variables_basic.py
+++ variables_basic.py
@@ -1,5 +1,6 @@
 STR_XPATH_BTN_BASIC_MENU = '//*[@id="basic_example"]/span/i'
 
+# Demo element name format: Type_indicate-category_element-type_demo-name_demo-id_demo-element
 # Simple Form Demo
 STR_XPATH_BTN_SIMPLE_FORM = '//*[@id="basic"]/div/a[1]'
 STR_XPATH_INPUT_SIMPLE_SINGLE_MSG = '//*[@id="user-message"]'
@@ -9,3 +10,12 @@ STR_XPATH_INPUT_SIMPLE_TWO_VAL_A = '//*[@id="sum1"]'
 STR_XPATH_INPUT_SIMPLE_TWO_VAL_B = '//*[@id="sum2"]'
 STR_XPATH_BTN_SIMPLE_TWO_TOTAL = '//*[@id="gettotal"]/button'
 STR_XPATH_TEXT_SIMPLE_TWO_SHOW_SUM = '//*[@id="displayvalue"]'
+# Check Box Demo
+STR_XPATH_BTN_CHECK_BOX = '//*[@id="basic"]/div/a[2]'
+STR_XPATH_CHECKBOX_CHECK_SINGLE_CHECKBOX = '//*[@id="isAgeSelected"]'
+STR_XPATH_CHECKBOX_CHECK_SINGLE_SHOW_MSG = '//*[@id="txtAge"]'
+STR_XPATH_CHECKBOX_CHECK_MULTIPLE_OPTION_1 = '//*[@id="easycont"]/div/div[2]/div[2]/div[2]/div[1]/label/input'
+STR_XPATH_CHECKBOX_CHECK_MULTIPLE_OPTION_2 = '//*[@id="easycont"]/div/div[2]/div[2]/div[2]/div[2]/label/input'
+STR_XPATH_CHECKBOX_CHECK_MULTIPLE_OPTION_3 = '//*[@id="easycont"]/div/div[2]/div[2]/div[2]/div[3]/label/input'
+STR_XPATH_CHECKBOX_CHECK_MULTIPLE_OPTION_4 = '//*[@id="easycont"]/div/div[2]/div[2]/div[2]/div[4]/label/input'
+STR_XPATH_BTN_CHECK_MULTIPLE_ALL = '//*[@id="check1"]'
